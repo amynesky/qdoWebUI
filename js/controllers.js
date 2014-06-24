dc45643b2a8f9975c34e00d027e01b5b7e08a471
amynesky <alnesky@lbl.gov>
2014-06-24 16:14:01 -0700

    Blank templates, only GETs no POSTing. Does not successfully GET from REST API

--- js/controllers.js
+++ js/controllers.js
@@ -30,25 +30,41 @@ epbControllers.controller('userhomeCtrl',
         var queues = QueueFactory.query({"username": $scope.username});
         queues.$promise.then(function(data){
           $scope.queues = data.queues; 
+          var pointer = 0;
+          for ( var i=0; i < data.queues.length; i++) { 
+            pointer += 1;
+          };
+          $scope.nqueues = pointer;
+
+          $scope.ntasks = function(queue) {
+              var totaltasks = 0;
+              for ( var state in queue["ntasks"]) {      
+                totaltasks += queue["ntasks"][state];
+              };
+              return totaltasks;
+          };
+          
+          $scope.percent = function(queue, status) {
+              var totaltasks = $scope.ntasks(queue);
+              if (totaltasks > 0){
+                return 100.0 * queue["ntasks"][status] / totaltasks;
+              }else{
+                return 0;
+              };
+          };
+          $scope.succeeded = function(queue) {return $scope.percent(queue, "Succeeded") == 100;};
+          $scope.failed = function(queue) {return $scope.percent(queue, "Failed") > 0;};
+          $scope.inProgress = function(queue) {return !$scope.succeeded(queue) && !$scope.failed(queue) && $scope.ntasks(queue) > 0;};
         });
-      
-      $scope.percent = function(queue, status) {
-          var totaltasks = 0;
-          for ( var state in queue["ntasks"]) {      
-            totaltasks += queue["ntasks"][state];
-          };
-          if (totaltasks > 0){
-            return 100.0 * queue["ntasks"][status] / totaltasks;
-          }else{
-            return 0;
-          };
-      };
-      $scope.succeeded = function(queue) {return $scope.percent(queue, "Succeeded") == 100;};
-      $scope.failed = function(queue) {return $scope.percent(queue, "Failed") > 0;};
-      $scope.inProgress = function(queue) {return !$scope.succeeded(queue) && !$scope.failed(queue);};
+
+
 
         
     });
+
+
+
+
 
 
 
@@ -68,20 +84,27 @@ epbControllers.controller('queueCtrl',
             };
           };
           $scope.queue = data.queues[pointer]; 
+
+          $scope.ntasks = function(queue) {
+              var totaltasks = 0;
+              for ( var state in queue["ntasks"]) {      
+                totaltasks += queue["ntasks"][state];
+              };
+              return totaltasks;
+          };
+          
+          $scope.percent = function(queue, status) {
+              var totaltasks = $scope.ntasks(queue);
+              if (totaltasks > 0){
+                return 100.0 * queue["ntasks"][status] / totaltasks;
+              }else{
+                return 0;
+              };
+          };
+          $scope.succeeded = function(queue) {return $scope.percent(queue, "Succeeded") == 100;};
+          $scope.failed = function(queue) {return $scope.percent(queue, "Failed") > 0;};
+          $scope.inProgress = function(queue) {return !$scope.succeeded(queue) && !$scope.failed(queue) && $scope.ntasks(queue) > 0;};
         });
         
-        $scope.percent = function(queue, status) {
-          var totaltasks = 0;
-          for ( var state in queue["ntasks"]) {      
-            totaltasks += queue["ntasks"][state];
-          };
-          if (totaltasks > 0){
-            return 100.0 * queue["ntasks"][status] / totaltasks;
-          }else{
-            return 0;
-          };
-        };
-        $scope.succeeded = function(queue) {return $scope.percent(queue, "Succeeded") == 100;};
-        $scope.failed = function(queue) {return $scope.percent(queue, "Failed") > 0;};
-        $scope.inProgress = function(queue) {return !$scope.succeeded(queue) && !$scope.failed(queue);};
+
     });
